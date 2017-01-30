6759a71571c9bbe367fa22a6bc481376ddc2d0c7
Julian David <juliangalvis75@gmail.com>
2017-01-29 20:25:44 -0500

    Adding function to gell all friend ascyn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var url = require('url');
 var express = require('express');
 var querystring = require('querystring');
-//var asyn = require('async');
+var async = require('async');
 var authenticator = require('./authenticator');
 var config = require('./config');
 var app = express();
@@ -84,6 +84,78 @@ app.get('/friends', function(req, res) {
 
           res.send(data);
       });
+});
+
+// List all friends
+app.get('/allfriends', function(req, res) {
+	async.waterfall([
+		// Get friends' IDs
+		function(cb) {
+			var cursor = -1;
+			var ids = [];
+
+			// Get IDs by traversing the cursored collection
+			async.whilst(function() {
+				return cursor != 0;
+			}, function(cb) {
+				authenticator.get('https://api.twitter.com/1.1/friends/ids.json?' + querystring.stringify({user_id: req.cookies.twitter_id, cursor: cursor}),
+					req.cookies.access_token, req.cookies.access_token_secret,
+					function(error, data) {
+						if (error) {
+							return res.status(400).send(error);
+						}
+
+						data = JSON.parse(data);
+						cursor = data.next_cursor_str;
+						ids = ids.concat(data.ids);
+
+						cb();
+					});
+			}, function(error) {
+				if (error) {
+					return res.status(500).send(error);
+				}
+
+				cb(null, ids);
+			});
+		},
+		// Get friends' data
+		function(ids, cb) {
+			// Returns up to 100 ids starting from 100*i
+			var getHundredthIds = function(i) {
+				return ids.slice(100*i, Math.min(ids.length, 100*(i+1)));
+			}
+			var requestsNeeded = Math.ceil(ids.length/100);
+
+			async.times(requestsNeeded, function(n, next) {
+				var url = 'https://api.twitter.com/1.1/users/lookup.json?' + querystring.stringify({user_id: getHundredthIds(n).join(',')});
+
+				authenticator.get(url,
+					req.cookies.access_token, req.cookies.access_token_secret,
+					function(error, data) {
+						if (error) {
+							return res.status(400).send(error);
+						}
+
+						var friends = JSON.parse(data);
+						next(null, friends);
+				});
+			},
+			function (err, friends) {
+				// Flatten friends array
+				friends = friends.reduce(function(previousValue, currentValue, currentIndex, array) {
+				  return previousValue.concat(currentValue);
+				}, []);
+
+				// Sort the friends alphabetically by name
+				friends.sort(function(a, b) {
+					return a.name.toLowerCase().localeCompare(b.name.toLowerCase());
+				});
+
+				res.send(friends);
+			});
+		}
+	]);
 });
 
 // Start listening for requests
